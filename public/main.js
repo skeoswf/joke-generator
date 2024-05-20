ac9be28ea75c98d0913751e1169092e0adabb5af
Skeo <skeo051600@gmail.com>
2024-05-19 20:15:55 -0500

    Add complete and perfect functionality!!

--- public/main.js
+++ public/main.js
@@ -3,12 +3,37 @@ import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
 
 const jokeButton = document.querySelector('#joke-button');
+const jokeWrapper = document.querySelector('#joke-wrapper');
+
+//  prettier-ignore
+const jokeEndpoint = 'https://v2.jokeapi.dev/joke/Programming?safe-mode&type=twopart';
+
+//  prettier-ignore
+const jokeCall = () => new Promise((resolve, reject) => {
+  fetch(jokeEndpoint, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+let punchFunny = '';
 
 jokeButton.addEventListener('click', () => {
   jokeButton.classList.toggle('secondary-state');
   if (jokeButton.classList.contains('secondary-state')) {
     jokeButton.innerHTML = 'get the FUCKING punchline';
+    jokeCall().then((data) => {
+      jokeWrapper.innerHTML = data.setup;
+      punchFunny = data.delivery;
+    });
   } else {
     jokeButton.innerHTML = 'wanna hear anootherrrr funny jokeee :p';
+
+    jokeWrapper.innerHTML = `${punchFunny}`;
   }
 });
