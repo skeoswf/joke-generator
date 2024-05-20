ceffb2a5ba6e8c3e7983433e8a44d2dafefb5580
Skeo <skeo051600@gmail.com>
2024-05-19 19:03:03 -0500

    Change button text on toggle

--- public/main.js
+++ public/main.js
@@ -1,3 +1,14 @@
 // USE WITH FIREBASE AUTH
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
+
+const jokeButton = document.querySelector('#joke-button');
+
+jokeButton.addEventListener('click', () => {
+  jokeButton.classList.toggle('secondary-state');
+  if (jokeButton.classList.contains('secondary-state')) {
+    jokeButton.innerHTML = 'get the FUCKING punchline';
+  } else {
+    jokeButton.innerHTML = 'wanna hear anootherrrr funny jokeee :p';
+  }
+});
